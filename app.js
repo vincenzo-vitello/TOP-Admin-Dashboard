f27a09bc6e5c78604f8d3919aec21f0cc2c51b7a
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2024-12-17 17:39:47 +0100

    added dark mode switch and project end

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 let projectActionsWrapper = document.querySelectorAll(".project_actions");
-let project_actions = document.createElement("div");
-
+const themeToggler = document.getElementById("theme_toggler");
+const body = document.querySelector("body");
+const notificationBell = document.getElementById("notification_bell");
+const searchIcon = document.getElementById("search_icon");
 projectActionsWrapper.forEach((element) => {
-  let project_actions = `<image src="./assets/star-plus-outline.svg" />
+  let project_actions = `
+    <image src="./assets/star-plus-outline.svg" />
     <image src="./assets/eye-plus-outline.svg" />
-    <image src="./assets/source-branch.svg" />`;
+    <image src="./assets/source-branch.svg" />
+  `;
   element.innerHTML = project_actions;
 });
+
+themeToggler.onclick = () => {
+  const currentTheme = body.getAttribute("data-theme");
+
+  if (currentTheme === "dark") {
+    body.setAttribute("data-theme", "light");
+    themeToggler.setAttribute("src", "./assets/light-mode.svg");
+    notificationBell.setAttribute("src", "./assets/notifications.svg");
+    searchIcon.setAttribute("src", "./assets/search.svg");
+    projectActionsWrapper.forEach((element) => {
+      let project_actions = `
+        <image src="./assets/star-plus-outline.svg" />
+        <image src="./assets/eye-plus-outline.svg" />
+        <image src="./assets/source-branch.svg" />
+      `;
+      element.innerHTML = project_actions;
+    });
+  } else {
+    body.setAttribute("data-theme", "dark");
+    themeToggler.setAttribute("src", "./assets/dark-mode.svg");
+    notificationBell.setAttribute("src", "./assets/notification-dark-mode.svg");
+    searchIcon.setAttribute("src", "./assets/search-dark.svg");
+    projectActionsWrapper.forEach((element) => {
+      let project_actions = `
+        <image src="./assets/star-plus-outline-dark.svg" />
+        <image src="./assets/eye-plus-outline-dark.svg" />
+        <image src="./assets/source-branch-dark.svg" />
+      `;
+      element.innerHTML = project_actions;
+    });
+  }
+};
